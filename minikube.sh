57b61b02e87447d1623012ad757c4e378f419888
ahmadqayyum <52801818+ahmadqayyum@users.noreply.github.com>
2019-07-19 11:30:11 +0500

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -3,7 +3,7 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
 apt-get update -y
-apt-get install -y minikube 
+install minikube /usr/local/bin 
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 kubectl version
